f4ed608e76f6ac2747a85b02c7d1dfb04f648d45
heris <herisjanss@gmail.com>
2023-03-03 02:51:28 +0800

    Upload Terbaru

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Heris Janis
+          <h1>Heris Janss</h1>
         </p>
         <a
           className="App-link"
